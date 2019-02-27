ffdd0c1d0cfc339ab7d8542fdd0e8d566e5d44e7
captiosus <captiosus@users.noreply.github.com>
2019-02-26 20:03:47 -0500

    storing turbo nodes in redis

--- vasilisk/coverage/turbo.py
+++ vasilisk/coverage/turbo.py
@@ -1,9 +1,16 @@
 import json
+import redis
 
 
 class TurboCoverage(object):
     def __init__(self):
-        pass
+        self.redis = redis.Redis(host='localhost', port=6379, db=0,
+                                 decode_responses=True)
+        self.optimizations = self.redis.hgetall('optimizations')
+        self.combinations = set(self.redis.smembers('combinations'))
+        print(self.optimizations)
+        print(self.combinations)
+        self.misses = 0
 
     def parse(self, turbo_json):
         with open(turbo_json, 'r') as f:
@@ -21,13 +28,30 @@ class TurboCoverage(object):
                 control_ids.add(edge['target'])
 
         control = []
+        combination = []
         for node in nodes:
             if node['id'] in control_ids:
-                control.append(str(node['title']))
+                title = str(node['title'])
+                control.append(title)
+                if title not in self.optimizations:
+                    id = str(len(self.optimizations))
+                    self.optimizations[title] = id
+                    self.redis.hset('optimizations', title, id)
+
+                combination.append(self.optimizations[title])
+
+        combination = ','.join(combination)
+
+        if combination not in self.combinations:
+            self.combinations.add(combination)
+            self.redis.sadd('combinations', combination)
+            self.misses = 0
+        else:
+            self.misses += 1
 
         return control
 
 
 if __name__ == '__main__':
     opt = TurboCoverage()
-    print(opt.parse())
+    print(opt.parse('turbo-f-0.json'))
