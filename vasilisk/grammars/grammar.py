bc70a4d821cfc8d40718bb11280544699575f6b3
Sai <svv232@nyu.edu>
2019-04-10 01:05:53 -0400

    generate function additions

--- vasilisk/grammars/grammar.py
+++ vasilisk/grammars/grammar.py
@@ -5,7 +5,7 @@ from base import BaseGrammar
 
 
 class Grammar(BaseGrammar):
-    def __init__(self, grammars):
+    def __init__(self, grammars, repeat=4):
         self.xref_re = r"""(
             (?P<type>\+|!|@)(?P<xref>[a-zA-Z0-9:_]+)(?P=type)|
             %repeat%\(\s*(?P<repeat>.+?)\s*(,\s*"(?P<separator>.*?)")?\s*(,\s*(?P<nodups>nodups))?\s*\)|
@@ -15,6 +15,8 @@ class Grammar(BaseGrammar):
         )"""
 
         self.corpus = {}
+        self.repeat_max = repeat
+        self.max_recursions = 100000
 
         for grammar in grammars:
             grammar_name = os.path.basename(grammar).replace('.dg', '')
@@ -62,45 +64,96 @@ class Grammar(BaseGrammar):
                         for xref_subrule in xref_subrules:
                             replacements.append(xref_l + xref_subrule + xref_r)
 
-                    print(replacements)
+                    #print(replacements)
 
-    def generate_test(self, action_list, controls_list = None, variable_list=None):
+    def generate_test(self, grammar_list, name="regexp", path = None):
         """ 
         loads a list of grammars and checks regex to output code
         """
+        if name not in self.corpus:
+            self.corpus[name] = self.parse(path)
+
         test_case = ""
 
-        action_rules = self.parse("templates/actions.dg")
-        control_rules = self.parse("templates/controls.dg")
-        variable_rules = self.parse("templates/variables.dg")
-
-        rules = (
-            action_rules,
-            control_rules,
-            variable_rules
-        )
-
-        for action in action_list:
+        for grammar in grammar_list:
             rule, index = map(lambda x: int(x) if x.isnumeric() else x, 
-                    action.split(":"))
-            if index > len(action_rules[rule]):
+                    grammar.split(":"))
+            if index > len(self.corpus[name][rule]):
                 raise ValueError(f"grammar index is too high for {rule}")
             
-            test_case += f"{self.generate(action_rules[rule][index], rules, 'action')}; "
+            test_case += f"{self.generate(self.corpus[name][rule][index], 'regexp')};"
+            print(test_case)
             break
 
+    def generate(self, rule,  grammar, cache = None, recurse_count = 0):
+        out, end = [], 0
+        if cache is None:
+            cache = {}
 
-    def generate(self, rule, rules, grammar):
-        action, control, variable = rules
-        if grammar == "action":
-            print(action["regex"])
-            print(rule);
-        elif grammar == "control":
-            pass
-        elif grammar == "variable":
-            pass
-        else:
-            raise ValueError("Unknown grammar in generate function")
+        for k in cache.items():
+            print(k)
+
+
+        
+        if recurse_count == self.max_recursions:
+            return False
+        
+        if rule in cache:
+            return cache[rule]
+
+        token = rule.replace("\\n", "\n")
+        for m in re.finditer(self.xref_re, token, re.VERBOSE | re.DOTALL):
+            if m.start(0) > end:
+                end = m.end(0)
+            elif m.group("repeat") is not None:
+                repeat, separator, nodups = m.group("repeat", "separator", "nodups")
+                #print(repeat, separator, nodups)
+                if separator is None:
+                    separator = ""
+                if nodups is None:
+                    nodups = ""
+
+                if ":" in separator:
+                    grammar = separator.split(":")[0]
+                
+                for i in range(self.repeat_max):
+                    res = self.generate(repeat, grammar, cache, recurse_count + 1)
+                    if res:
+                        out.append(res)
+                    else:
+                        # use Roy's code to use another grammar index instead of the
+                        # one specified previously
+                        pass
+
+                cache[rule] = separator.join(out)
+                return cache[rule]
+
+            elif m.group("choices") is not None:
+                choices = m.group("choices")
+            else:
+                if "+" in rule:
+                    if ":" in rule:
+                        grammar = rule.split(":")[0].strip("+")
+                        gramm = rule.split(":")[1].strip("+")
+                    else:
+                        gramm = rule.strip("+")
+                    print(grammar, gramm, self.corpus[grammar][gramm][7] )
+
+                    gramm = self.generate(self.corpus[grammar][gramm][7], grammar, cache, recurse_count + 1)
+                else:
+                    gramm = rule
+                    # have to use Roy's code that covers basic cases and sucessfully
+                    # parse and make the grammar assuming it is a nonunique case that won't be passed back
+                    pass
+
+                if gramm:
+                    cache[rule] = gramm
+                    return cache[rule]
+                else:
+                    # same case as before pick another grammar index in a rule and try again
+                    pass
+                #startval, endval = m.group("start", "end")
+        return out
         
 if __name__ == '__main__':
     current_dir = os.path.dirname(os.path.realpath(__file__))
@@ -112,7 +165,7 @@ if __name__ == '__main__':
         for grammar in os.listdir(os.path.join(dependencies))
     ]
 
-    test_grammars = ["regex:1", "regex:0", "regex:2", "regex:3", "regex:4"]
+    test_grammars = ["pattern:0"]
     
     actions = os.path.join(templates, 'actions.dg')
     controls = os.path.join(templates, 'controls.dg')
@@ -120,6 +173,5 @@ if __name__ == '__main__':
 
     grammar = Grammar(grammar_deps + [actions, controls, variables])
     grammar.parse_xrefs()
+    
     grammar.generate_test(test_grammars)
-
-    print(grammar.corpus['regexp'])
